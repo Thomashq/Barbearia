dc7538ac3b57046a5a7181445dcc23ead01f1891
Thomas Henrique <thomashenriqq@gmail.com>
2024-03-26 23:16:00 -0300

    rotas de update services

--- ws/src/routes/services.routes.js
+++ ws/src/routes/services.routes.js
@@ -6,6 +6,56 @@ const barberShop = require('../models/barberShop');
 const Archives = require('../models/archive')
 const Services = require('../models/service');
 
+
+router.put('/:id', async (req, res) => {
+    const busBoy = new BusBoy({ headers: req.headers });
+    busBoy.on('finish', async () => {
+        try {
+            const { barberShopId, service } = req.body;
+            let errors = [];
+            let arquivos = [];
+
+            if (req.files && Object.keys(req.files) > 0) {
+                for (let key of Object.keys(req.files)) {
+                    const file = req.files[key];
+                    const nameParts = file.name.split('.');
+                    const fileName = `${new Date().getTime()}.${nameParts[nameParts.length - 1]}`;
+
+                    const path = `services/${barberShopId}/${fileName}`;
+
+                    const response = await aws.uploadToS3(file, path);
+                    if (response.error) {
+                        errors.push({ error: true, message: response.message });
+                    }
+                    else {
+                        arquivos.push(path);
+                    }
+                }
+            }
+
+            if(errors.length > 0){
+                res.json(errors[0]);
+                return false;
+            }
+            const jsonService = JSON.parse(service);
+            await Services.findByIdAndUpdate(req.params.id, jsonService);
+            //criação do arquivo 
+            archives = arquivos.map((archive) => ({
+                referenciaId: req.params.id,
+                model: 'Service',
+                caminho: archive
+            }));
+
+            await Archives.insertMany(archives);
+            res.json({error: false})
+        }
+        catch (err) {
+            res.json({ error: true, message: err.message })
+        }
+    });
+    req.pipe(busBoy);
+});
+
 //Não é possível criar arquivos via json, usarei formdata
 router.post('/', async (req, res) => {
     const busBoy = new BusBoy({ headers: req.headers });
